2fd28cc42b079c6f9f364deb80b883fe2fcc5e16
unicom <info@unicomsales.com>
2019-02-13 13:50:16 -0500

    Setting up e2e with circleci

---
+++ e2e/src/xxx-app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './xxx-app.po';
+
+describe('workspace-project App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getTitleText()).toEqual('Welcome to stackexchange1!');
+  });
+});
