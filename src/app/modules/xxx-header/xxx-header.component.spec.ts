5f4f5f95257107dff8c9a56c855b397d83d057b0
unicom <info@unicomsales.com>
2019-02-18 21:53:11 -0500

    xxx-header-component unit tests all 100%, new mock-flex-layout.

--- src/app/modules/xxx-header/xxx-header.component.spec.ts
+++ src/app/modules/xxx-header/xxx-header.component.spec.ts
@@ -1,14 +1,16 @@
 import {CommonModule} from '@angular/common';
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-import {FlexLayoutModule} from '@angular/flex-layout';
+import {FlexLayoutModule, MediaObserver} from '@angular/flex-layout';
 import {MatToolbarModule} from '@angular/material';
 
+import {MockMediaObserver} from '../../../mocks/angular/mock-flex-layout';
 import {XxxHeaderComponent} from './xxx-header.component';
 import {XxxSearchBoxComponent} from '../xxx-search-box/mock-xxx-search-box.component';
 
 describe('XxxHeaderComponent', () => {
   let component: XxxHeaderComponent;
   let fixture: ComponentFixture<XxxHeaderComponent>;
+  let mediaObserver: MediaObserver;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -20,6 +22,9 @@ describe('XxxHeaderComponent', () => {
         CommonModule,
         FlexLayoutModule,
         MatToolbarModule
+      ],
+      providers: [
+        {provide: MediaObserver, useClass: MockMediaObserver}
       ]
     }).compileComponents();
   }));
@@ -27,6 +32,7 @@ describe('XxxHeaderComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(XxxHeaderComponent);
     component = fixture.componentInstance;
+    mediaObserver = TestBed.get(MediaObserver);
   });
 
   it('should create', () => {
