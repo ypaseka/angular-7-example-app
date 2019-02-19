4071c7f154cef25bd7bc3b5f296d30efa790be13
unicom <info@unicomsales.com>
2019-02-18 23:42:31 -0500

    xxx-stack-exchange-answers-component unit tests all 100%.

--- src/app/modules/xxx-stack-exchange-answers/xxx-stack-exchange-answers.component.spec.ts
+++ src/app/modules/xxx-stack-exchange-answers/xxx-stack-exchange-answers.component.spec.ts
@@ -1,9 +1,10 @@
-import {async, ComponentFixture, fakeAsync, flush, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {MatButtonModule, MatIconModule, MatProgressSpinnerModule, MatSnackBarModule} from '@angular/material';
 import {ActivatedRoute} from '@angular/router';
 import {RouterTestingModule} from '@angular/router/testing';
+import {of} from 'rxjs';
 
 import {MockActivatedRouteWithId, mockRouteParamId} from '../../../mocks/angular/mock-activated-route';
 import {MockXxxAlertService} from '../../library/xxx-alert/mock-xxx-alert.service';
@@ -19,10 +20,12 @@ import {XxxStateStoreService} from '../../library/xxx-state-store/xxx-state-stor
 describe('XxxStackExchangeAnswersComponent', () => {
   let component: XxxStackExchangeAnswersComponent;
   let fixture: ComponentFixture<XxxStackExchangeAnswersComponent>;
+  let route: ActivatedRoute;
   let spyAlertService: jasmine.Spy;
   let spyDataService: jasmine.Spy;
   let spyEventMgrService: jasmine.Spy;
-  let spyStateStoreService: jasmine.Spy;
+  let spyStateStoreGetItem: jasmine.Spy;
+  let spyStateStorePutItem: jasmine.Spy;
   let xxxAlertService: XxxAlertService;
   let xxxDataService: XxxDataService;
   let xxxEventMgrService: XxxEventMgrService;
@@ -75,22 +78,31 @@ describe('XxxStackExchangeAnswersComponent', () => {
     xxxEventMgrService = TestBed.get(XxxEventMgrService);
     spyEventMgrService = spyOn(xxxEventMgrService, 'handleEvent');
     xxxStateStoreService = TestBed.get(XxxStateStoreService);
-    spyStateStoreService = spyOn(xxxStateStoreService, 'putItem');
+    spyStateStoreGetItem = spyOn(xxxStateStoreService, 'getItem');
+    spyStateStorePutItem = spyOn(xxxStateStoreService, 'putItem');
+    route = TestBed.get(ActivatedRoute);
   });
 
   it('should create', fakeAsync(() => {
     createComponent();
-    flush();
+    tick();
     expect(component).toBeTruthy();
   }));
 
   it('should get the question id from the route url id', fakeAsync(() => {
     createComponent();
-    flush();
+    tick();
     expect(spyDataService).toHaveBeenCalled();
     const url: string = spyDataService.calls.mostRecent().args[0];
     const isUrlCorrect = url.includes(mockRouteParamId);
     expect(isUrlCorrect).toBeTruthy();
+  }));
+
+  it('should handle missing question id from the route url id', fakeAsync(() => {
+    route.params = of({});
+    createComponent();
+    tick();
+    expect(spyDataService).not.toHaveBeenCalled();
   }));
 
   it('should run get question and get answers with success', fakeAsync(() => {
@@ -102,7 +114,7 @@ describe('XxxStackExchangeAnswersComponent', () => {
       }
     });
     createComponent();
-    flush();
+    tick();
     expect(component.isResult).toBeTruthy();
     expect(component.isError).toBeFalsy();
     expect(component.isBusy).toBeFalsy();
@@ -117,7 +129,7 @@ describe('XxxStackExchangeAnswersComponent', () => {
       alertMessage = msg;
     });
     createComponent();
-    flush();
+    tick();
     expect(spyAlertService).toHaveBeenCalled();
     expect(alertType).toBe('warn');
     expect(alertMessage).toBe('Given Question Id Not Found');
@@ -138,7 +150,7 @@ describe('XxxStackExchangeAnswersComponent', () => {
       alertMessage = msg;
     });
     createComponent();
-    flush();
+    tick();
     expect(spyAlertService).toHaveBeenCalled();
     expect(alertType).toBe('warn');
     expect(alertMessage).toBe('No Answers Found For Given Question Id');
@@ -153,7 +165,7 @@ describe('XxxStackExchangeAnswersComponent', () => {
       }
     });
     createComponent();
-    flush();
+    tick();
     expect(component.isError).toBeTruthy();
     expect(component.isBusy).toBeFalsy();
   }));
@@ -167,7 +179,7 @@ describe('XxxStackExchangeAnswersComponent', () => {
       }
     });
     createComponent();
-    flush();
+    tick();
     expect(component.isError).toBeTruthy();
     expect(component.isBusy).toBeFalsy();
   }));
@@ -175,8 +187,36 @@ describe('XxxStackExchangeAnswersComponent', () => {
   it('should decode html entities', fakeAsync(() => {
     let result: string;
     createComponent();
-    flush();
+    tick();
     result = component.decodeHtmlEntities('&#62;');
     expect(result).toBe('>');
+  }));
+
+  it('should run onClickBackToQuestions', fakeAsync(() => {
+    createComponent();
+    tick();
+    component.onClickBackToQuestions();
+    expect(spyEventMgrService).toHaveBeenCalled();
+    const eventId = spyEventMgrService.calls.mostRecent().args[0];
+    expect(eventId).toEqual('routeQuestions');
+  }));
+
+  it('should run checkForQuestions on create with no value', fakeAsync(() => {
+    createComponent();
+    tick();
+    expect(spyStateStoreGetItem).toHaveBeenCalled();
+    const stateKey = spyStateStoreGetItem.calls.mostRecent().args[0];
+    expect(stateKey).toEqual('questionsRoute');
+    expect(component.isQuestions).toBeFalsy();
+  }));
+
+  it('should run checkForQuestions on create with value', fakeAsync(() => {
+    spyStateStoreGetItem.and.returnValue('x');
+    createComponent();
+    tick();
+    expect(spyStateStoreGetItem).toHaveBeenCalled();
+    const stateKey = spyStateStoreGetItem.calls.mostRecent().args[0];
+    expect(stateKey).toEqual('questionsRoute');
+    expect(component.isQuestions).toBeTruthy();
   }));
 });
